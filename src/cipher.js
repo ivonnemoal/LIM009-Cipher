4cc09b0ca12f19783b524fe390f634c3191c9045
Tu-username= <ivonne.morales.alarcon@gmail.com>
2019-03-06 08:29:31 -0500

    proyecto finalizado

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,56 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
+	encode: (offset, name) => {
+		let codeCipher;
+		let position;
+		let textResult = '';
+		for (let i = 0; i < name.length; i++) {
+			if(name[i] === ' '){
+        textResult += ' ';
+      }else{
+				const Ascii = name.charCodeAt(i);
+				if(Ascii > 90){
+					position = Ascii - 97 + (offset % 26);
+					codeCipher = (position % 26) + 97;
+				}else{
+					position = Ascii - 65 + (offset % 26);
+					codeCipher = (position % 26) + 65;
+				}
+				const letterCipher = String.fromCharCode(codeCipher);
+				textResult += letterCipher;
+			}
+		}
+		return textResult;
+	},
+	decode: (offset, string) => {
+		let codeCipher, position;
+		let textResult = '';
+		for (let i = 0; i < string.length; i++) {
+			if(string[i] === ' '){
+				textResult += ' ';
+			}else{
+				const codeAscii = string.charCodeAt(i);
+				if(codeAscii > 90){
+					position = codeAscii - 97 - (offset % 26);
+					if(position < 0){
+						codeCipher = (position % 26) + 123;
+					}else{
+						codeCipher = (position % 26) + 97;
+					}
+				}else{
+					position = codeAscii - 65 - (offset % 26);
+					if(position < 0){
+						codeCipher = (position % 26) + 91;
+					}else{
+						codeCipher = (position % 26) + 65;
+					}
+				}
+				const letterCipher = String.fromCharCode(codeCipher);
+				textResult += letterCipher;
+			}
+		}
+		return textResult;
+	},
+
+	createCipherWithOffset:(offset, string) => {
+	}
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-/* Acá va tu código */
+const getDatos = (election) => {
+    const offset = parseInt(document.getElementById('number').value);
+    if(election){
+        const name = document.getElementById('text').value; 
+        document.getElementById('text').value = '';
+        document.getElementById('result').value = cipher.encode(offset, name);
+    }else{
+        const name = document.getElementById('result').value;
+        document.getElementById('result').value = '';
+        document.getElementById('text').value = cipher.decode(offset, name);
+    }
+}
